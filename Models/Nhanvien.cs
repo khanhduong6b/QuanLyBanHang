044240a958b17e223163c86bc28ce221b1f8cb8d
khanhduong6b <khanhduong6b@gmail.com>
2023-12-12 13:46:04 +0700

    fix: data-annotation for nhanvien model

--- Models/Nhanvien.cs
+++ Models/Nhanvien.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuanLyBanHang.Models;
 
 public partial class Nhanvien
 {
+    [DisplayName("Mã số nhân viên")]
+    [Required(ErrorMessage ="Chưa nhập mã số nhân viên")]
     public string Manv { get; set; } = null!;
-
+    [DisplayName("Tên nhân viên")]
+    [Required(ErrorMessage = "Chưa nhập tên nhân viên")]
     public string? Tennv { get; set; }
-
+    [DisplayName("Ngày sinh")]
+    [Required(ErrorMessage = "Chưa nhập ngày sinh nhân viên")]
     public DateTime? Ngaysinh { get; set; }
-
+    [DisplayName("Giới tính")]
+    [Required(ErrorMessage = "Chưa chọn giới tính")]
     public bool? Phai { get; set; }
-
+    [DisplayName("Địa chỉ")]
     public string? Diachi { get; set; }
 
     public string? Password { get; set; }
